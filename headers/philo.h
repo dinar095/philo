be70f0e84878f828fa849458a804b7cc0c5e5c3b
Desausag <dinar095@hotmail.com>
2021-07-28 18:36:15 +0300

    add libft

---
+++ headers/philo.h
@@ -0,0 +1,22 @@
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <unistd.h>
+# include "../libft/libft.h"
+typedef struct	s_start
+{
+	int num_of_philo;
+	int	time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int must_eat;
+	int num_of_finish_of_filo;
+	int finish;
+	
+}				t_start;
+
+typedef struct 	s_philo
+{
+
+}				t_philo;
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include "headers/philo.h"
 
-int main() {
-	printf("Hello, World!\n");
+void str_error(char *str, int err)
+{
+	ft_putstr_fd(str, 1);
+	exit(err);
+}
+int	parse_args(char **argv, int argc)
+{
+	int i;
+
+	i = 0;
+
+	while (i++ < argc)
+		printf("%s\n", argv[i]);
+	return (0);
+}
+int main(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+		str_error("Error arguments\n", 1);
+	if (parse_args(argv, argc))
+		str_error("Error arguments\n", 1);
 	return 0;
 }
