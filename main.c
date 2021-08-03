89343a608075bbfab7529b7eade67ef8939373e3
Desausag <dinar095@hotmail.com>
2021-08-03 16:59:18 +0300

    add check for numreic argv

--- main.c
+++ main.c
@@ -4,23 +4,22 @@ void str_error(char *str)
 {
 	ft_putstr_fd(str, 1);
 }
+
 int	is_num(char **argv)
 {
-	int i;
 	int j;
 
-	i = 0;
-
-	while (*argv)
+	argv++;
+	while (*(++argv))
 	{
 		j = -1;
-		while (argv[i][++j])
-			if (!(ft_isdigit(argv[i][j])))
+		while ((*argv)[++j])
+			if (!(ft_isdigit((*argv)[j])))
 				return (0);
-		argv++;
 	}
 	return (1);
 }
+
 int	parse_args(int argc, char **argv)
 {
 	int i;
